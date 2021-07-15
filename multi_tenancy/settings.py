29fe9576295a69a7ec0e2d55b4c71e4a340ac050
thenyakamau <51839588+thenyakamau@users.noreply.github.com>
2021-07-15 15:49:51 +0300

    Update settings.py

--- multi_tenancy/settings.py
+++ multi_tenancy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dqh_30r+f6jze$xf$^y+olbg$n_a9l9az46bveo5_6!c%!wldt'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
